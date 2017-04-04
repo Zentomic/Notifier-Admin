b5253ac6cb5c5c11c4e2d2b9ac8a94bd76ce6dfc
ngoc tuan nguyen <ngoctuan82@gmail.com>
2017-04-04 10:17:08 -0700

    deploy heroku zentomic-webnotifier better

--- deploy-heroku.bat
+++ deploy-heroku.bat
@@ -1,10 +1,10 @@
 git add .
 
-git commit -m "deploy heroku zentomic-notifier better"
+git commit -m "deploy heroku zentomic-webnotifier better"
 
 heroku login
 
-heroku git:remote -a zentomic-notifier
+heroku git:remote -a zentomic-webnotifier
 
 git remote -v
 
